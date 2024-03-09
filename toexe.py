6d1c3490ba39b413f50e0c8c99b85218b3e8ce5a
DLmaster <DLmaster@dlchurch.onmicrosoft.com>
2024-03-09 14:56:12 +0800

    增加发行信息重新打包

--- toexe.py
+++ toexe.py
@@ -20,6 +20,6 @@
 
 import os
 
-os.system("pyinstaller -F -w --version-file res/info.txt -i res/AUTO_MAA.ico manage.py")
-os.system("pyinstaller -F -w --version-file res/info.txt -i res/AUTO_MAA.ico run.py")
-os.system("pyinstaller -F -w --version-file res/info.txt -i res/AUTO_MAA.ico AUTO_MAA.py")
+os.system("pyinstaller -F --version-file res/info.txt -i res/AUTO_MAA.ico manage.py")
+os.system("pyinstaller -F --version-file res/info.txt -i res/AUTO_MAA.ico run.py")
+os.system("pyinstaller -F --version-file res/info.txt -i res/AUTO_MAA.ico AUTO_MAA.py")

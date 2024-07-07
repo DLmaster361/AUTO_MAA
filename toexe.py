20108551397467f1b0a2c539ba72cf909763a8cd
DLmaster <DLmaster@dlchurch.onmicrosoft.com>
2024-07-07 12:43:40 +0800

    重构，GUI初步开发

--- toexe.py
+++ toexe.py
@@ -20,6 +20,4 @@
 
 import os
 
-os.system("pyinstaller -F --version-file res/info.txt -i res/AUTO_MAA.ico manage.py")
-os.system("pyinstaller -F --version-file res/info.txt -i res/AUTO_MAA.ico run.py")
-os.system("pyinstaller -F --version-file res/info.txt -i res/AUTO_MAA.ico AUTO_MAA.py")
+os.system("pyinstaller -F --version-file res/info.txt --icon=res/AUTO_MAA.ico gui.py")

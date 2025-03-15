57a102e26492f853c15089da81bb731ffe884d1a
DLmaster <DLmaster_361@163.com>
2025-03-16 00:37:10 +0800

    feat(notice): 为全新UI提供匹配字段

---
+++ notice_formatting.py
@@ -0,0 +1,13 @@
+title = "AUTO_MAA私域邮箱"
+markdown = """
+## AUTO_MAA私域邮箱已上线！
+
+- **AUTO_MAA私域邮箱是什么？**AUTO_MAA私域邮箱是基于阿里云企业邮箱的`automaa.xyz`域名邮箱。
+
+- **AUTO_MAA私域邮箱有啥优点？**嗯，域名比较特别，看起来专业点？
+
+- **AUTO_MAA私域邮箱怎么用于通知服务？**AUTO_MAA私域邮箱的登录页为[阿里邮箱企业版](https://qiye.aliyun.com)，SMTP 服务器地址为`smtp.qiye.aliyun.com`，授权码为`登录密码`，其他使用方法与普通邮箱一致。
+
+- **怎么申请AUTO_MAA私域邮箱？**发送申请邮件到`DLmaster_361@automaa.xyz`，要求邮件中包含：`申请邮箱的类别（用户/项目组）`、`邮箱名（纯英文）`、`安全手机号`、`申请原因`。邮箱创建成功后，将会返回一封包含`初始密码`的回执邮件。
+"""
+print(f'"{title}": "{markdown.replace("\n",r"\n")}"')

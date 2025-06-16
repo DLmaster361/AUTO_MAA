0572caa528deb72bc97c42e0b169292823b1e8a8
DLmaster361 <DLmaster_361@163.com>
2025-06-17 03:18:55 +0800

    fix: 固定certifi版本号

--- app/core/config.py
+++ app/core/config.py
@@ -576,7 +576,7 @@ class MaaPlanConfig(LQConfig):
 
 class AppConfig(GlobalConfig):
 
-    VERSION = "4.3.11.0"
+    VERSION = "4.3.12.0"
 
     gameid_refreshed = Signal()
     PASSWORD_refreshed = Signal()

43eb9e0ef3990c59e0593c7ea3035c08e293fe43
ClozyA <aoxuan233@gmail.com>
2026-09-10 11:52:52 +0800

    chore(version): 更新版本至 v5.5.0-beta.4，包含新增、变更和修复

--- app/core/config.py
+++ app/core/config.py
@@ -252,7 +252,7 @@ def _parse_maa_drop_statistics(logs: list[str]) -> dict[str, dict[str, int]]:
 
 
 class AppConfig(GlobalConfig):
-    VERSION = "v5.5.0-beta.3"
+    VERSION = "v5.5.0-beta.4"
 
     def __init__(self) -> None:
         super().__init__()
